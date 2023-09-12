2a97037c5a95c7cb4bbe05aaa405e24178b0190e
Priaec <101358256+Priaec@users.noreply.github.com>
2023-09-12 01:05:57 -0700

    Border Gradient and change of colors

--- port/src/components/HeaderBar.js
+++ port/src/components/HeaderBar.js
@@ -6,10 +6,10 @@ class HeaderBar extends React.Component{
   render(){
     return (
       <div className='headerBar-container'>
-        <p className='headerBar-button-left-element' style={{color: Colors.darkBackground}}>Joshua Cohen</p>
-        <p className='headerBar-button' style={{color: Colors.darkBackground}}>About Me</p>
-        <p className='headerBar-button' style={{color: Colors.darkBackground}}>Projects</p>
-        <p className='headerBar-button' style={{color: Colors.darkBackground}}>Interests</p>
+        <p className='headerBar-button-left-element'>Joshua Cohen</p>
+        <p className='headerBar-button'>About Me</p>
+        <p className='headerBar-button'>Projects</p>
+        <p className='headerBar-button'>Interests</p>
         <p id='contact-button' className='headerBar-button'>Contact</p>
       </div>
     );
